8ece8f7b61fc07cb5e6541ed4118212100c39efd
SaiSomanchi <102578404+SaiSomanchi@users.noreply.github.com>
2022-03-30 15:02:27 +0530

    Add files via upload

---
+++ .dockerignore.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 29 23:13:00 2022
+
+@author: 703311085
+"""
+
+Dockerfile
+.git*
+.cache*
+**/*.pyc
+**/*.pyo
+**/*.pyd
+__pycache__
+train
+gcp_commands
+request_test
